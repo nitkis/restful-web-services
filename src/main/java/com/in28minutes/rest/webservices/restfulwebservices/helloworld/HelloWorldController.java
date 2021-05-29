869b997152bcccfeff825f27bb21ecdff6394b03
nitkis <nitin.123048@gmail.com>
2021-05-29 23:33:20 +0530

    second commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import com.in28minutes.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.web.bind.annotation.*;
+
+//controller
+@RestController
+public class HelloWorldController {
+
+//    get
+//    URI - /hello-world
+//    method -"Hello World"
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+//    hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean()
+    {
+        return new HelloWorldBean("Hello World");
+    }
+
+    //hello-world/path-variable/in28minutes
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name)
+    {
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+}
