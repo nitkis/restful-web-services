2d3b0d3a04634d8be65ad976bd8e470ba6ac0227
nitkis <nitin.123048@gmail.com>
2021-05-31 23:03:11 +0530

    ninth commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -22,7 +22,20 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id)
     {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user==null)
+            throw new UserNotFoundException("id-"+ id);
+
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id)
+    {
+        User user = service.deleteById(id);
+        if (user==null)
+            throw new UserNotFoundException("id-"+ id);
+
     }
 
     @PostMapping("/users")
